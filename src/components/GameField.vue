49f56126218cfbb85e57db554642c8831411c7e5
OleksiiMazur <oleksiimazur111@gmail.com>
2020-09-08 04:33:48 +0300

    Need to animate card

--- src/components/GameField.vue
+++ src/components/GameField.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="game-field">
     <div class="game-field__card-place">
-      <card :customers="customers"
-            :drugs="drugs"
-            :currentCustomer="currentCustomer"
-            v-touch:swipe.left="swipeLeft"
-            v-touch:swipe.top="swipeTop"
-            v-touch:swipe.right="swipeRight"
-      />
+      <transition name="card-change">
+        <card :customers="customers"
+              :drugs="drugs"
+              :currentCustomer="currentCustomer"
+              v-touch:swipe.left="swipeLeft"
+              v-touch:swipe.top="swipeTop"
+              v-touch:swipe.right="swipeRight"
+        />
+      </transition>
     </div>
     <div class="drugs-list">
       <div class="btn drugs-list__item"
@@ -57,3 +59,16 @@ export default {
   },
 };
 </script>
+
+<style lang="scss">
+.card-change-enter-active {
+  transition: opacity 1.2s ease-in;
+}
+.card-change-leave-active {
+  transition: opacity 0.7s ease-out;
+}
+.card-change-enter,
+.card-change-leave-to {
+  opacity: 0;
+}
+</style>
